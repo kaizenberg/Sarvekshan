56096823a68e8022c26556bd8873f0f56bd980f8
kaizenberg <kaizenberg.inbox@gmail.com>
2020-04-06 21:06:14 +0530

    Added formatting for whatsapp msg

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,8 +9,8 @@ var json = {
         "es": "COVID-19 स्व-निर्धारण"
     },
     "completedHtml": {
-        "default": "Thank you!",
-        "es": "धन्यवाद!"
+        "default": "<p>Thank you</p><p><a id=\"whatsAppLinkId\" href=\"#\">Send your answers to MyGov WhatsApp Helpline</a></p>",
+        "es": "<p>धन्यवाद</p><p><a id=\"whatsAppLinkId\" href=\"#\">अपने जवाब MyGov WhatsApp हेल्पलाइन पर भेजें</a></p>"
     },
     "loadingHtml": {
         "default": "Please wait",
@@ -24,12 +24,11 @@ var json = {
                     "type": "html",
                     "name": "Intro",
                     "html": {
-                        "default": "<P>This self-assessment suggests further steps depending on your level of risk of exposure to COVID-19 (Novel Coronavirus).</p><p>You can complete this form for yourself or others who are not able.</P>\n<p>\n<a id=\"changeLanguageAnchor\" href=\"javascript:toggleLanguage();\">हिंदी के लिए यहाँ क्लिक करें<a/>\n</p>",
-                        "es": "<P>यह स्व-निर्धारण COVID-19 (नोवेल कोरोनावायरस) संक्रमण के जोखिम के आपके स्तर के आधार पर आगे के चरणों का सुझाव देता है।</p><p>आप इस प्रपत्र को अपने या अन्य लोगों के लिए पूरा कर सकते हैं जो सक्षम नहीं हैं।</P>\n<p>\n<a id=\"changeLanguageAnchor\" href=\"javascript:toggleLanguage();\">Click here for English<a/>\n</p>"
+                        "default": "<P>This self-assessment form suggests further steps depending on your level of risk of exposure to COVID-19 (Novel Coronavirus).</p><p>You can complete this form for yourself or others who are not able.</P>\n<a id=\"changeLanguageAnchor\" href=\"javascript:toggleLanguage();\">हिंदी<a/>",
+                        "es": "<P>यह स्व-निर्धारण प्रपत्र COVID-19 (नोवेल कोरोनावायरस) संक्रमण के जोखिम के आपके स्तर के आधार पर आगे के चरणों का सुझाव देता है।</p><p>आप इस प्रपत्र को अपने या अन्य लोगों के लिए पूरा कर सकते हैं जो सक्षम नहीं हैं।</P>\n<a id=\"changeLanguageAnchor\" href=\"javascript:toggleLanguage();\">English</a>"
                     }
                 }
-            ],
-            "readOnly": true
+            ]
         },
         {
             "name": "Page 1",
@@ -38,8 +37,8 @@ var json = {
                     "type": "html",
                     "name": "Q1",
                     "html": {
-                        "default": "Are you experiencing any of the following symptoms:<ul><li>\nSudden difficulty in breathing, such as:<ul><li>gasping for air while talking</li><li>shortness of breath at rest</li><li>difficulty in breathing after lying down</li></ul></li><li>Chest pain</li><li> Difficulty in waking up</li> <li>Fainting, or losing consciousness </li><li>Difficulty in performing daily routine</li><li>Increased symptoms of existing respiratory illness</li></ul>",
-                        "es": "क्या आप निम्नलिखित लक्षणों में से किसी का अनुभव कर रहे हैं:<ul><li>\nअचानक साँस लेने में कठिनाई, जैसे:<ul><li>बात करते समय हवा के लिए हांफना</li><li>बिना किसी हलचल के भी\nसांस फूलना</li><li>लेटने के बाद सांस लेने में कठिनाई</li></ul></li><li>छाती में दर्द</li><li>नींद से जागने में कठिनाई</li><li>बेहोशी, या होश खो देना</li><li>दैनिक दिनचर्या करने में कठिनाई</li><li>मौजूदा श्वसन बीमारी के बढ़ते लक्षण</li><ul>"
+                        "default": "<p>Are you experiencing any of the following symptoms:</p><ul><li>Sudden difficulty in breathing, such as:<ul><li>gasping for air while talking</li><li>shortness of breath at rest</li><li>difficulty in breathing after lying down</li></ul></li><li>Chest pain</li><li>Difficulty in waking up</li><li>Fainting, or losing consciousness</li><li>Difficulty in performing daily routine</li><li>Increased symptoms of existing respiratory illness</li></ul>",
+                        "es": "<p>क्या आप निम्नलिखित लक्षणों में से किसी का अनुभव कर रहे हैं:</p><ul><li>अचानक साँस लेने में कठिनाई, जैसे:<ul><li>बात करते समय हवा के लिए हांफना</li><li>बिना किसी हलचल के भी सांस फूलना</li><li>लेटने के बाद सांस लेने में कठिनाई</li></ul></li><li>छाती में दर्द</li><li>नींद से जागने में कठिनाई</li><li>बेहोशी, या होश खो देना</li><li>दैनिक दिनचर्या करने में कठिनाई</li><li>मौजूदा श्वसन बीमारी के बढ़ते लक्षण</li><ul>"
                     }
                 },
                 {
@@ -80,8 +79,8 @@ var json = {
                     "type": "html",
                     "name": "Q2",
                     "html": {
-                        "default": "Are you experiencing any of the following symptoms:\n<ul>\n<li>\nFever\n</li><li>Cough\n</li><li>Shortness of breath\n</li><li>Sore throat\n</li><li>Muscle aches\n</li><li>Cold, Runny nose</li>\n</ul>",
-                        "es": "क्या आप निम्नलिखित लक्षणों में से किसी का अनुभव कर रहे हैं:\n<ul>\n<li>\nबुखार\n</li><li>खांसी\n</li><li>सांस लेने में दिक्कत\n</li><li>गले में खराश\n</li><li>मांसपेशियों में दर्द\n</li><li>सर्दी, बहती नाक</li>\n</ul>"
+                        "default": "<p>Are you experiencing any of the following symptoms:</p><ul><li>Fever\n</li><li>Cough</li><li>Shortness of breath</li><li>Sore throat</li><li>Muscle aches</li><li>Cold, Runny nose</li></ul>",
+                        "es": "<p>क्या आप निम्नलिखित लक्षणों में से किसी का अनुभव कर रहे हैं:</p><ul><li>बुखार</li><li>खांसी</li><li>सांस लेने में दिक्कत</li><li>गले में खराश</li><li>मांसपेशियों में दर्द</li><li>सर्दी, बहती नाक</li></ul>"
                     }
                 },
                 {
@@ -124,8 +123,8 @@ var json = {
                     "type": "html",
                     "name": "Q3",
                     "html": {
-                        "default": "Have you come in close contact with people who are:\n<ul><li>infected with COVID-19</li> <li>getting tested for COVID-19</li> <li>symptomatic and have traveled </li><li>\nin contact with biological laboratory material</li><li>\nproviding care to infected people, including family members, caretakers, and health workers who have not used protective equipment</li></ul>",
-                        "es": "क्या आप ऐसे लोगों के निकट संपर्क में आए हैं, जो:\n<ul><li>COVID-19 से संक्रमित है</li><li>\nCOVID-19 के लिए जांच की जा रही है</li><li>\nरोगसूचक है और यात्रा की है</li><li>\nजैविक प्रयोगशाला सामग्री के संपर्क में है</li><li>\nCOVID -19 संक्रमित लोगों को देखभाल प्रदान की, जिनमें परिवार के सदस्य, देखभाल करने वाले और स्वास्थ्य कार्यकर्ता शामिल हैं, जिन्होंने सुरक्षात्मक उपकरण का उपयोग नहीं किया है"
+                        "default": "<p>Have you come in close contact with people who are:</p><ul><li>infected with COVID-19</li><li>getting tested for COVID-19</li><li>symptomatic and have traveled</li><li>in contact with biological laboratory material</li><li>\nproviding care to infected people, including family members, caretakers, and health workers who have not used protective equipment</li></ul>",
+                        "es": "<p>क्या आप ऐसे लोगों के निकट संपर्क में आए हैं, जो:</p><ul><li>COVID-19 से संक्रमित है</li><li>COVID-19 के लिए जांच की जा रही है</li><li>रोगसूचक है और यात्रा की है</li><li>जैविक प्रयोगशाला सामग्री के संपर्क में है</li><li>COVID -19 संक्रमित लोगों को देखभाल प्रदान की, जिनमें परिवार के सदस्य, देखभाल करने वाले और स्वास्थ्य कार्यकर्ता शामिल हैं, जिन्होंने सुरक्षात्मक उपकरण का उपयोग नहीं किया है</li></ul>"
                     }
                 },
                 {
@@ -168,8 +167,8 @@ var json = {
                     "type": "html",
                     "name": "Q4",
                     "html": {
-                        "default": "In the last 30 days have you been outside of the country or your state, or attended any social gatherings, or visited crowded public places?",
-                        "es": "पिछले 30 दिनों में आप देश या अपने राज्य से बाहर रहे हैं, या किसी भी सामाजिक समारोहों में भाग लिया है, या भीड़भाड़ वाले सार्वजनिक स्थानों का दौरा किया है?"
+                        "default": "<p>In the last 30 days have you been outside of the country or your state, or attended any social gatherings, or visited crowded public places?</p>",
+                        "es": "</p>पिछले 30 दिनों में आप देश या अपने राज्य से बाहर रहे हैं, या किसी भी सामाजिक समारोहों में भाग लिया है, या भीड़भाड़ वाले सार्वजनिक स्थानों का दौरा किया है?</p>"
                     }
                 },
                 {
@@ -213,7 +212,7 @@ var json = {
                     "name": "R1",
                     "html": {
                         "default": "<h1 style=\"color:lightgreen;\">Low Risk</h1>\n<p><strong><ul><li>You have low risk of COVID-19 infection</li><li>Follow social isolation & suggested measures</li><li>Do self assessment again after 7 days</li></ul></strong></p>\n<p>\nDial the national helpline number below if your risk level changes.\n<ul><li>+91-11-23978046</li><li>1075</li><li>+91 90131 51515 (WhatsApp)</li></ul>\n<p>For more information on COVID-19, please visit the website of <a href=\"https://www.mohfw.gov.in/\">Ministry of Health & Family Welfare</a>.</p>",
-                        "es": "<h1 style=\"color:lightgreen;\">कम जोखिम </h1>\n<p><strong><ul><li>\nसामाजिक अलगाव और सुझाए गए उपायों का पालन करें</li><li>7 दिनों के बाद फिर से स्व-निर्धारण करें</li></ul></strong></p>\n<P>\nयदि आपका जोखिम स्तर बदलता है तो नीचे दिए गए राष्ट्रीय सहायता क्रमांक पर तुरंत संपर्क करें।\n<ul><li>+91-11-23978046</li><li>1075</li></ul>\n<p>COVID-19 के बारे में अधिक जानकारी के लिए, कृपया <a href=\"https://www.mohfw.gov.in/\"> स्वास्थ्य और परिवार कल्याण मंत्रालय</a> की वेबसाइट पर जाएँ।</p>"
+                        "es": "<h1 style=\"color:lightgreen;\">कम जोखिम </h1>\n<p><strong><ul><li>\nसामाजिक अलगाव और सुझाए गए उपायों का पालन करें</li><li>7 दिनों के बाद फिर से स्व-निर्धारण करें</li></ul></strong></p>\n<P>\nयदि आपका जोखिम स्तर बदलता है तो नीचे दिए गए राष्ट्रीय सहायता क्रमांक पर तुरंत संपर्क करें।\n<ul><li>+91-11-23978046</li><li>1075</li><li>WhatsApp: +91 90131 51515</li></ul>\n<p>COVID-19 के बारे में अधिक जानकारी के लिए, कृपया <a href=\"https://www.mohfw.gov.in/\"> स्वास्थ्य और परिवार कल्याण मंत्रालय</a> की वेबसाइट पर जाएँ।</p>"
                     }
                 }
             ],
@@ -229,7 +228,7 @@ var json = {
                     "name": "R2",
                     "html": {
                         "default": "<h1 style=\"color:skyblue;\">Medium Risk</h1>\n<p><strong><ul><li>You have moderate risk of COVID-19 infection</li><li>Immediately isolate yourself to avoid infecting others for 7 days</li><li>It is NOT necessary for you to get tested for COVID-19 </li><li>Take the self assessment again after 7 days</li></ul></strong></p>\n<p>\nDial the national helpline number given below if your risk level changes.\n<ul><li>+91-11-23978046</li><li>1075</li><li>+91 90131 51515 (WhatsApp)</li></ul>\n<p>For more information on COVID-19, please visit the website of <a href=\"https://www.mohfw.gov.in/\">Ministry of Health & Family Welfare</a>.</p>",
-                        "es": "<h1 style=\"color:skyblue;\">मध्यम जोखिम </h1>\n<p><strong><ul><li>दूसरे लोगों को संक्रमित करने से बचने के लिए तुरंत 7 दिनों के लिए खुद को अलग कर लें</li><li>COVID-19 का परीक्षण करवाना आपके लिए आवश्यक नहीं है</li><li>7 दिनों के बाद फिर से स्व-निर्धारण करें</li></ul></strong></p>\n<P>\nयदि आपका जोखिम स्तर बदलता है तो नीचे दिए गए राष्ट्रीय सहायता दूरध्वनी क्रमांक पर तुरंत संपर्क करें।\n<ul><li>+91-11-23978046</li><li>1075</li></ul>\n<p>COVID-19 के बारे में अधिक जानकारी के लिए, कृपया <a href=\"https://www.mohfw.gov.in/\"> स्वास्थ्य और परिवार कल्याण मंत्रालय</a> की वेबसाइट पर जाएँ।</p>"
+                        "es": "<h1 style=\"color:skyblue;\">मध्यम जोखिम </h1>\n<p><strong><ul><li>दूसरे लोगों को संक्रमित करने से बचने के लिए तुरंत 7 दिनों के लिए खुद को अलग कर लें</li><li>COVID-19 का परीक्षण करवाना आपके लिए आवश्यक नहीं है</li><li>7 दिनों के बाद फिर से स्व-निर्धारण करें</li></ul></strong></p>\n<P>\nयदि आपका जोखिम स्तर बदलता है तो नीचे दिए गए राष्ट्रीय सहायता दूरध्वनी क्रमांक पर तुरंत संपर्क करें।\n<ul><li>+91-11-23978046</li><li>1075</li><li>WhatsApp: +91 90131 51515</li></ul>\n<p>COVID-19 के बारे में अधिक जानकारी के लिए, कृपया <a href=\"https://www.mohfw.gov.in/\"> स्वास्थ्य और परिवार कल्याण मंत्रालय</a> की वेबसाइट पर जाएँ।</p>"
                     }
                 }
             ],
@@ -244,8 +243,8 @@ var json = {
                     "type": "html",
                     "name": "R5",
                     "html": {
-                        "default": "<h1 style=\"color:orange;\">High Risk</h1>\n<p><strong><ul><li>You have higher risk of COVID-19 infection</li><li>Immediately isolate yourself to avoid infecting others</li><li>It is necessary for you to get tested for COVID-19</li></ul></strong></p>\n<p>\nDial the national helpline number given below immediately.\n<ul><li>+91-11-23978046</li><li>1075</li></ul>\n<p>Or, send your answers to MyGov WhatsApp Helpline. <a id=\"whatsAppLinkId\" href=\"#\">Click<img src=\"assets/img/whatsapp.png\"/></a></p>\n<p>For more information on COVID-19, please visit the website of <a href=\"https://www.mohfw.gov.in/\">Ministry of Health & Family Welfare</a>.</p><script type=\"text/javascript\">loadLink();</script>",
-                        "es": "<h1 style=\"color:orange;\">उच्च जोखिम</h1>\n<p><strong><ul><li>दूसरे लोगों को संक्रमित करने से बचने के लिए तुरंत खुद को अलग कर लें</li><li>COVID-19 का परीक्षण करवाना आपके लिए आवश्यक है</li></ul></strong></p>\n<P>\nनीचे दिए गए राष्ट्रीय सहायता दूरध्वनी क्रमांक पर तुरंत संपर्क करें।\n<ul><li>+91-11-23978046</li><li>1075</li></ul>\n<p>या, अपने जवाब MyGov WhatsApp हेल्पलाइन पर भेजें। <a id=\"whatsAppLinkId\" href=\"#\">क्लिक करें<img src=\"assets/img/whatsapp.png\"/></a></p>\n<p>COVID-19 के बारे में अधिक जानकारी के लिए, कृपया <a href=\"https://www.mohfw.gov.in/\"> स्वास्थ्य और परिवार कल्याण मंत्रालय</a> की वेबसाइट पर जाएँ।</p><script type=\"text/javascript\">loadLink();</script>"
+                        "default": "<h1 style=\"color:orange;\">High Risk</h1>\n<p><strong><ul><li>You have higher risk of COVID-19 infection</li><li>Immediately isolate yourself to avoid infecting others</li><li>It is necessary for you to get tested for COVID-19</li></ul></strong></p>\n<p>\nDial the national helpline number given below immediately.\n<ul><li>+91-11-23978046</li><li>1075</li><li>+91 90131 51515 (WhatsApp)</li></ul>\n<p>For more information on COVID-19, please visit the website of <a href=\"https://www.mohfw.gov.in/\">Ministry of Health & Family Welfare</a>.</p>",
+                        "es": "<h1 style=\"color:orange;\">उच्च जोखिम</h1>\n<p><strong><ul><li>दूसरे लोगों को संक्रमित करने से बचने के लिए तुरंत खुद को अलग कर लें</li><li>COVID-19 का परीक्षण करवाना आपके लिए आवश्यक है</li></ul></strong></p>\n<P>\nनीचे दिए गए राष्ट्रीय सहायता दूरध्वनी क्रमांक पर तुरंत संपर्क करें।\n<ul><li>+91-11-23978046</li><li>1075</li><li>WhatsApp: +91 90131 51515</li></ul>\n<p>COVID-19 के बारे में अधिक जानकारी के लिए, कृपया <a href=\"https://www.mohfw.gov.in/\"> स्वास्थ्य और परिवार कल्याण मंत्रालय</a> की वेबसाइट पर जाएँ।</p>"
                     }
                 }
             ],
@@ -260,8 +259,8 @@ var json = {
                     "type": "html",
                     "name": "R4",
                     "html": {
-                        "default": "<h1 style = \"color: red;\">Seek Urgent Help</h1>\n<strong><p>Dial the national helpline number given below immediately.</strong>\n<ul><li>+91-11-23978046</li><li>1075</li></ul>\n<p>Or, send your answers to MyGov WhatsApp Helpline. <a id=\"whatsAppLinkId\" href=\"#\">Click<img src=\"assets/img/whatsapp.png\"/></a></p>\n<p>For more information about COVID-19, please visit the website of the <a href=\"https://www.mohfw.gov.in/\"> Ministry of Health and Family Welfare</a>.</p><script type=\"text/javascript\">loadLink();</script>",
-                        "es": "<h1 style=\"color:red;\">तत्काल मदद लें</h1>\n<p><strong>नीचे दिए गए राष्ट्रीय सहायता दूरध्वनी क्रमांक पर तुरंत संपर्क करें।</strong>\n<ul><li>+91-11-23978046</li><li>1075</li></ul>\n<p>या, अपने जवाब MyGov WhatsApp हेल्पलाइन पर भेजें। <a id=\"whatsAppLinkId\" href=\"#\">क्लिक करें<img src=\"assets/img/whatsapp.png\"/></a></p>\n<p>COVID-19 के बारे में अधिक जानकारी के लिए, कृपया <a href=\"https://www.mohfw.gov.in/\"> स्वास्थ्य और परिवार कल्याण मंत्रालय</a> की वेबसाइट पर जाएँ।</p><script type=\"text/javascript\">loadLink();</script>"
+                        "default": "<h1 style = \"color: red;\">Seek Urgent Help</h1>\n<strong><p>Dial the national helpline number given below immediately.</strong>\n<ul><li>+91-11-23978046</li><li>1075</li><li>+91 90131 51515 (WhatsApp)</li></ul>\n<p>For more information about COVID-19, please visit the website of the <a href=\"https://www.mohfw.gov.in/\"> Ministry of Health and Family Welfare</a>.</p>",
+                        "es": "<h1 style=\"color:red;\">तत्काल मदद लें</h1>\n<p><strong>नीचे दिए गए राष्ट्रीय सहायता दूरध्वनी क्रमांक पर तुरंत संपर्क करें।</strong>\n<ul><li>+91-11-23978046</li><li>1075</li><li>WhatsApp: +91 90131 51515</li></ul>\n<p>COVID-19 के बारे में अधिक जानकारी के लिए, कृपया <a href=\"https://www.mohfw.gov.in/\"> स्वास्थ्य और परिवार कल्याण मंत्रालय</a> की वेबसाइट पर जाएँ।</p>"
                     }
                 }
             ],
@@ -274,21 +273,22 @@ var json = {
     "showQuestionNumbers": "off",
     "startSurveyText": {
         "default": "Start",
-        "es": "शुरू करें"
+        "es": "शुरू"
     },
     "pagePrevText": {
-        "default": "Back",
-        "es": "पीछे जाएं"
+        "default": "Prev",
+        "es": "पीछे"
     },
     "pageNextText": {
-        "es": "आगे बढ़ें",
+        "es": "आगे",
         "default": "Next"
     },
     "completeText": {
         "default": "Finish",
         "es": "समाप्त"
     },
-    "requiredText": ""
+    "requiredText": "",
+    "firstPageIsStarted": true
 };
 
 window.survey = new Survey.Model(json);
