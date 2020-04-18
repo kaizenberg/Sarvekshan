c56e8e9e24f61ea092c6cef8ec71aa62ff41f912
kaizenberg <kaizenberg.inbox@gmail.com>
2020-04-18 22:31:02 +0530

    UI Enhancements

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,64 @@
-localforage.keys().then(function (keys) {
-    keys.forEach(function (key) {
-        $('#savedPassesList').append(
-            `<div>
-                <div id="card:${key}" class="card bg-light text-dark shadow rounded card-hover-pass">
-                    <div class="card-body">
-                        <i>${key}</i>
-                        <button type="button" class="close" aria-label="Close">
-                            <span id="btn:${key}" class="deleteButton" aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                </div>
-                <br/>
-            </div>`);
+function loadSavedPasses() {
+    localforage.keys().then(function (keys) {
+        if (keys.length == 0) {
+            $('#savedPassesList').append('<div class="font-weight-light" style="margin-left:15px"><small>No Records!</small></div>');
+        }
+        else {
+            keys.forEach(function (key) {
+                $('#savedPassesList').append(
+                    `<div>
+                        <div id="card:${key}" class="card text-dark shadow-sm rounded card-hover-pass">
+                            <div class="card-body">
+                                <i class="fa fa-download downloadButton" aria-hidden="true"></i>
+                                <i>${key}</i>
+                                <button type="button" class="close" aria-label="Close">
+                                    <span id="btn:${key}" class="deleteButton" aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                        </div>
+                        <br/>
+                    </div>`);
+            });
+        }
+    }).catch(function (err) {
+        console.log(err);
     });
-}).catch(function (err) {
-    console.log(err);
+}
+
+loadSavedPasses();
+
+$(".custom-file-input").on("change", function (file) {
+    var fileName = $(this).val().split("\\").pop();
+    var filePath = $(this).siblings(".custom-file-label");
+
+    var input = file.target;
+    var reader = new FileReader();
+    reader.onload = function () {
+        var payload = reader.result;
+        if (payload.startsWith("data:image/png")) {
+            //Here should be the code to save the data into your database
+            localforage.setItem(fileName, reader.result).then(function (value) {
+                // Do other things once the value has been saved.
+                console.log(fileName + " : Saved to local storage");
+                filePath.addClass("selected").html("");
+                $('#savedPassesList').html("");
+                loadSavedPasses();
+                $('#uploadQrCodeModal').modal('hide');
+            }).catch(function (err) {
+                // This code runs if there were any errors
+                console.log(err);
+            });
+        }
+        else {
+            filePath.addClass("selected").html("Invalid file!");
+            console.log(fileName + " : is not valid image");
+        }
+    };
+    reader.readAsDataURL(input.files[0]);
+});
+
+$('#uploadCard').click(function () {
+    $('#uploadQrCodeModal').modal('show');
 });
 
 $(document).on("click", ".deleteButton", function (event) {
@@ -22,6 +66,7 @@ $(document).on("click", ".deleteButton", function (event) {
 });
 
 $(document).on("click", ".card-hover-pass", function (event) {
+    if (event.target && event.target.className.startsWith('deleteButton')) return;
     var key = event.currentTarget.id.split(':')[1];
     localforage.getItem(key, function (err, value) {
         if (err) {
@@ -29,6 +74,7 @@ $(document).on("click", ".card-hover-pass", function (event) {
             return;
         }
         $('#ePassKey').text(key);
+        $('#qrCodeViewLink').attr('src', value);
         $('#qrCodeView').attr('src', value);
         $('#savedQrCodeModal').modal('show');
         console.log(key + " : retrieved from local db");
@@ -39,8 +85,14 @@ function removePass(element) {
     if (window.confirm("Do you really want to delete this ePass?")) {
         var key = element.id.split(':')[1];
         $(element).parent().parent().parent().parent().remove();
-        localforage.removeItem().then(function () {
+        localforage.removeItem(key).then(function () {
             console.log(key + ' is deleted from local db!');
+
+            localforage.keys().then(function (keys) {
+                if (keys.length == 0) {
+                    $('#savedPassesList').append('<div class="font-weight-light" style="margin-left:15px"><small>No Records!</small></div>');
+                }
+            });
         }).catch(function (err) {
             console.log(err);
         });
--- assets/js/verify.js
+++ assets/js/verify.js
@@ -1,18 +1,16 @@
-let currentUserId;
-var lastPeerId = null;
-var peer = null; // Own peer object
-var peerId = null;
-var conn = null;
+$('#scannedDataModel').on('shown.bs.modal', function () {
+    $("#scannedData").html("");
+})
 
 function onQRCodeScanned(scannedText) {
-    $("#scannedData").html("");
     var html = "";
     try {
         jQuery.each(JSON.parse(scannedText), function (name, val) {
-            if (name === "Device ID") currentUserId = val;
+            if (name === "Device ID") remoteDeviceId = val;
             html += "<div>" + name + ": " + val + "</div>";
         });
         $("#scannedData").append(html);
+        $('#scannedDataModel').modal('show');
     }
     catch (ex) {
         console.log("Scanned data is erroneous", ex);
@@ -74,79 +72,20 @@ function JsQRScannerReady() {
     }
 }
 
-document.getElementById('eVerify').addEventListener('click', function () {
-    if (currentUserId === null) return;
 
-    function join() {
-        // Close old connection
-        if (conn) {
-            conn.close();
-        }
+//--------------eVerification Logic----------------
 
-        // Create connection to destination peer specified in the input field
-        conn = peer.connect(recvIdInput.value, {
-            reliable: true
-        });
+let remoteDeviceId;
+var lastPeerId = null;
+var peer = null; // Own peer object
+var peerId = null;
+var conn = null;
 
-        conn.on('open', function () {
-            status.innerHTML = "Connected to: " + conn.peer;
-            console.log("Connected to: " + conn.peer);
-
-            // Check URL params for comamnds that should be sent immediately
-            var command = getUrlParam("command");
-            if (command)
-                conn.send(command);
-        });
-        // Handle incoming data (messages only since this is the signal sender)
-        conn.on('data', function (data) {
-            addMessage("<span class=\"peerMsg\">Peer:</span> " + data);
-        });
-        conn.on('close', function () {
-            status.innerHTML = "Connection closed";
-        });
-    };
-
-    /**
-     * Get first "GET style" parameter from href.
-     * This enables delivering an initial command upon page load.
-     *
-     * Would have been easier to use location.hash.
-     */
-    function getUrlParam(name) {
-        name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-        var regexS = "[\\?&]" + name + "=([^&#]*)";
-        var regex = new RegExp(regexS);
-        var results = regex.exec(window.location.href);
-        if (results == null)
-            return null;
-        else
-            return results[1];
-    };
-
-    //Generate TOTP
-    var totp = new jsOTP.totp();
-    var timeCode = totp.getOtp(60, 8);
-    console.log(timeCode);
-    console.log(totp.getOtp(60, 8));
-
-    //Open WebRTC Connection
-    var peer = new Peer();
-    var conn = peer.connect(currentUserId);
-
-    conn.on('open', function () {
-        // here you have conn.id
-        conn.send(timeCode);
-    });
-
-    peer.on('connection', function (conn) {
-        conn.on('data', function (data) {
-            conn.close();
-            totp.timeCode(60, 8) === data;
-        });
-    });
-
+function initialize() {
     // Create own peer object with connection to shared PeerJS server
-    peer = new Peer(myUniqueId);
+    peer = new Peer(null, {
+        debug: 2
+    });
 
     peer.on('open', function (id) {
         // Workaround for peer.reconnect deleting previous id
@@ -157,25 +96,11 @@ document.getElementById('eVerify').addEventListener('click', function () {
             lastPeerId = peer.id;
         }
 
-        console.log('ID: ' + peer.id);
-    });
-
-    peer.on('connection', function (c) {
-        // Allow only a single connection
-        if (conn) {
-            c.on('open', function () {
-                c.send("Already connected to another client");
-                setTimeout(function () { c.close(); }, 500);
-            });
-            return;
-        }
-
-        conn = c;
-        console.log("Connected to: " + conn.peer);
-        ready();
+        console.log('Your Device ID: ' + peer.id);
     });
 
     peer.on('disconnected', function () {
+        $('#verificationMsg').text("Connection problem. Please retry.");
         console.log('Connection lost. Please reconnect');
 
         // Workaround for peer.reconnect deleting previous id
@@ -186,11 +111,65 @@ document.getElementById('eVerify').addEventListener('click', function () {
 
     peer.on('close', function () {
         conn = null;
+        $('#verificationMsg').text("Connection destroyed. Please refresh");
         console.log('Connection destroyed');
     });
 
     peer.on('error', function (err) {
         console.log(err);
-        alert('' + err);
+        $('#verificationMsg').text("Error occured. Refresh the page.");
     });
+}
+
+function verify(remoteAddress) {
+    // Close old connection
+    if (conn) {
+        conn.close();
+    }
+
+    // Create connection to destination peer specified in the input field
+    conn = peer.connect(remoteAddress, {
+        reliable: true
+    });
+
+    conn.on('open', function () {
+        status.innerHTML = "Connected to: " + conn.peer;
+        console.log("Connected to: " + conn.peer);
+
+        //Generate TOTP
+        var totp = new jsOTP.totp();
+        var timeCode = totp.getOtp(60, 8);
+        console.log(timeCode);
+        conn.send(timeCode);
+    });
+
+    // Handle incoming data (messages only since this is the signal sender)
+    conn.on('data', function (data) {
+        if (totp.timeCode(60, 8) === data)
+            alert("Valid ePass");
+        else
+            alert("Invalid ePass!");
+        conn.close();
+    });
+
+    conn.on('close', function () {
+        status.innerHTML = "Connection closed";
+    });
+};
+
+function getUrlParam(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regexS = "[\\?&]" + name + "=([^&#]*)";
+    var regex = new RegExp(regexS);
+    var results = regex.exec(window.location.href);
+    if (results == null)
+        return null;
+    else
+        return results[1];
+};
+
+$('#eVerify').click(function () {
+    if (remoteDeviceId === null) return;
+    initialize();
+    verify(remoteDeviceId);
 });
